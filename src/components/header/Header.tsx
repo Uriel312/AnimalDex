5dce0052c639959e730b01a9b641358f65dea3f1
Uriel Chavez <tecwaroficial@gmail.com>
2024-08-06 11:04:34 -0600

    Modificando el titulo e icono de la web

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -12,6 +12,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.wrapper}>
+        <img onClick={goToHome} src="/AnimalDex.png" alt="" />
         <h1 onClick={goToHome}>AnimalDex</h1>
       </div>
     </header>
