9c621c8c9d327de816af34c42405ffc8a5605a06
Uriel Chavez <tecwaroficial@gmail.com>
2024-08-05 21:13:27 -0600

    Agregando el componente de animal empty

---
+++ src/components/animal-empty/AnimalEmpty.tsx
@@ -0,0 +1,31 @@
+import { useNavigate } from 'react-router-dom'
+import styles from './animal_empty.module.css'
+import { FaCirclePlus } from "react-icons/fa6";
+
+const AnimalEmpty = () => {
+
+  const navigation = useNavigate()
+
+
+  const goToSearch = () => {
+    navigation('/search')
+  }
+
+  return (
+    <div className={styles.container}>
+      <h4>
+        Actualmente no tienes animales en tu lista.
+      </h4>
+      <h2>
+        ¡Empieza a descubrir y agregar algunos nuevos!
+      </h2>
+
+      <button className={styles.button} onClick={goToSearch}>
+        <FaCirclePlus size='20px' />
+        Agregar
+      </button>
+    </div>
+  )
+}
+
+export default AnimalEmpty
