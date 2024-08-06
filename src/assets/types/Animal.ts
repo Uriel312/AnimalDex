927d141619f901eb47ff846765d5a8d24febc72a
Uriel Chavez <tecwaroficial@gmail.com>
2024-08-05 20:01:36 -0600

    agregando la interface de la respuesta de animales

---
+++ src/assets/types/Animal.ts
@@ -0,0 +1,20 @@
+export interface Animal {
+  common_name: string;
+  scientific_name: string;
+  animal_category: string;
+  weight: string;
+  height: string;
+  diet: string;
+  birth_type: string;
+  lifespan: string;
+  max_speed: string;
+  behavior_description: string;
+  habitat: string;
+  description: string;
+  conservation_status: string;
+  hostility_level: string;
+  poison: boolean;
+  dangerous_to_humans: boolean;
+  imagen: string;
+  id: string;
+}
